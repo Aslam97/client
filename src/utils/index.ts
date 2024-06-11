fec2313fd50a7b7605b6ee652a852679243c1bbe
Leo Lamprecht <leo@ronin.co>
2024-06-11 17:06:09 +0200

    Added `utils` export (#53)
    * Added new `/utils` export
    
    * Allow for batching queries in new export

---
+++ src/utils/index.ts
@@ -0,0 +1,5 @@
+import { runQueriesWithStorageAndHooks as runQueries } from '../queries';
+import { processStorableObjects } from '../storage';
+import { getBatchProxy, getSyntaxProxy } from '../syntax/utils';
+
+export { runQueries, processStorableObjects, getSyntaxProxy, getBatchProxy };
